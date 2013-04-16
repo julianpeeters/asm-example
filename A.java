fa7d166c2b27fc6ebb17d37ac76bc8cbc5be21d3
Julian Peeters <julianpeeters@gmail.com>
2013-04-15 23:12:55 -0700

    still a mess, splitting branches

---
+++ A.java
@@ -0,0 +1,25 @@
+/*
+class B {
+  static void hi() {
+    System.out.println("hi");
+  }
+}
+
+public class A extends B {
+  public static void main (String[] args) {
+     hi();
+  }
+}
+*/
+
+class B {
+  static void hi() {
+    System.out.println("hi");
+  }
+}
+
+public class C {
+  public static void main (String[] args) {
+     B
+  }
+}
