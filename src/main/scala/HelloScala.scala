593e4fb1b8eb47ff9b53698299e178274ce50b79
Julian Peeters <julianpeeters@gmail.com>
2013-04-23 00:08:16 -0700

    toolbox aint workin

--- src/main/scala/HelloScala.scala
+++ src/main/scala/HelloScala.scala
@@ -1,4 +1,6 @@
 import scala.reflect.runtime.universe._	
+import scala.tools.reflect._
+import scala.tools.reflect.ToolBox
 /*
 }
 class B {
@@ -8,9 +10,26 @@ class B {
 object HelloScala {
   def main(args: Array[String]): Unit = {
     //val b = new B
+
+    val cm = runtimeMirror(getClass.getClassLoader)
+    val tb  = cm.mkToolBox()
+    val expr = tb.parse("""class C {val hello = "Hello B!"}; scala.reflect.classTag[C].runtimeClass; new C""")
+      println(expr)
+    val eval = tb.eval(expr)
+      println(eval)
+    //  println(cm.classSymbol(eval.asInstanceOf[RuntimeClass]))
+
     val b = DumpLoader.loadClass("B", BDump.dump())
     //println(b.hello) //value hello is not a member of Class[?0]
     val b_ = b.getDeclaredConstructor().newInstance()
     //println(b_.hello)	//Value hello is not a member of Any
+
+
+    val cm_ = runtimeMirror(b.getClassLoader)
+    val tb_  = cm_.mkToolBox()
+    val expr_ = tb_.parse("""class C {val hello = "Hello B!"}; scala.reflect.classTag[C].runtimeClass""")
+      println(expr_)
+    val eval_ = tb_.eval(expr_)
+     // println(eval.asInstanceOf[RuntimeClass])
   }
 }
