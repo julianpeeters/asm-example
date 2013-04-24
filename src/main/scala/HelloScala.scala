5a409465da53e9bbcc330b5b7ca108942d506e8f
Julian Peeters <julianpeeters@gmail.com>
2013-04-23 21:08:40 -0700

    dynamic class not cast to a class type per se, but as a workaround, structural type is working for access to methods and as a parameter type

--- src/main/scala/HelloScala.scala
+++ src/main/scala/HelloScala.scala
@@ -3,24 +3,35 @@ import scala.tools.reflect._
 import scala.tools.reflect.ToolBox
 
 
-class B {
+class B extends {//MyRecord { //Error: class type required but AnyRef{val hello: String} found
   val hello = "Hello B!"
 }
 
 object HelloScala {
+  type MyRecord = { val hello: String}
   def main(args: Array[String]): Unit = {
     //val b = new B
 
-    struc(new B)
-  
-  //  val b = DumpLoader.loadClass("B", BDump.dump())
+  val b = DumpLoader.loadClass("B", BDump.dump())
     //println(b.hello) //value hello is not a member of Class[?0]
-   // val b_ = b.getDeclaredConstructor().newInstance()
+    val b_ = b.getDeclaredConstructor().newInstance().asInstanceOf[MyRecord]
     //println(b_.hello)	//Value hello is not a member of Any
+    val box = new Box[MyRecord](b_)
 
+   // struc(new B)
+    struc(b_)
+  //  println(new MyRecord)  //Error: class type required but AnyRef{val hello: String} found
+  
+  
   }
-  def struc(b: {val hello: String}):Unit = {
+
+  def struc(b: MyRecord):Unit = {
     println(b.hello)
+    //println(classOf[MyRecord]) //Error: class type required but AnyRef{val hello: String} found
   }
 
+
+  class Box[B](x: B) {
+        
+  }
 }
