5c0554133e2366d8e1bdbbb23fe4498bd02aab04
Julian Peeters <julianpeeters@gmail.com>
2013-05-07 19:47:11 -0700

    getting there

--- src/main/java/BDump.java
+++ src/main/java/BDump.java
@@ -10,17 +10,40 @@ FieldVisitor fv;
 MethodVisitor mv;
 AnnotationVisitor av0;
 
-cw.visit(V1_6, ACC_PUBLIC + ACC_SUPER, "B", null, "java/lang/Object", null);
+cw.visit(V1_6, ACC_PUBLIC + ACC_SUPER, "B", null, "java/lang/Object", new String[] { "A" });
 
 {
 av0 = cw.visitAnnotation("Lscala/reflect/ScalaSignature;", true);
-av0.visit("bytes", "\u0006\u0001y1A!\u0001\u0002\u0001\u000b\u0009\u0009!IC\u0001\u0004\u0003\u001daT-\u001c9usz\u001a\u0001a\u0005\u0002\u0001\rA\u0011qAC\u0007\u0002\u0011)\u0009\u0011\"A\u0003tG\u0006d\u0017-\u0003\u0002\u000c\u0011\u00091\u0011I\\=SK\u001aDQ!\u0004\u0001\u0005\u00029\u0009a\u0001P5oSRtD#A\u0008\u0011\u0005A\u0001Q\"\u0001\u0002\u0009\u000fI\u0001!\u0019!C\u0001'\u0005)\u0001.\u001a7m_V\u0009A\u0003\u0005\u0002\u001655\u0009aC\u0003\u0002\u00181\u0005!A.\u00198h\u0015\u0005I\u0012\u0001\u00026bm\u0006L!a\u0007\u000c\u0003\rM#(/\u001b8h\u0011\u0019i\u0002\u0001)A\u0005)\u00051\u0001.\u001a7m_\u0002\u0002");
+av0.visit("bytes", "\u0006\u0001\u00052A!\u0001\u0002\u0001\u000b\u0009\u0009!IC\u0001\u0004\u0003\u001daT-\u001c9usz\u001a\u0001aE\u0002\u0001\r1\u0001\"a\u0002\u0006\u000e\u0003!Q\u0011!C\u0001\u0006g\u000e\u000cG.Y\u0005\u0003\u0017!\u0011a!\u00118z%\u00164\u0007CA\u0007\u000f\u001b\u0005\u0011\u0011BA\u0008\u0003\u0005\u0005\u0009\u0005\"B\u0009\u0001\u0009\u0003\u0011\u0012A\u0002\u001fj]&$h\u0008F\u0001\u0014!\u0009i\u0001\u0001C\u0004\u0016\u0001\u0009\u0007I\u0011\u0001\u000c\u0002\u000b!,G\u000e\\8\u0016\u0003]\u0001\"\u0001G\u000f\u000e\u0003eQ!AG\u000e\u0002\u00091\u000cgn\u001a\u0006\u00029\u0005!!.\u0019<b\u0013\u0009q\u0012D\u0001\u0004TiJLgn\u001a\u0005\u0007A\u0001\u0001\u000b\u0011B\u000c\u0002\r!,G\u000e\\8!\u0001");
 av0.visitEnd();
 }
 // ATTRIBUTE ScalaSig
 {
 fv = cw.visitField(ACC_PRIVATE + ACC_FINAL, "hello", "Ljava/lang/String;", null, null);
 fv.visitEnd();
+}
+{
+fv = cw.visitField(ACC_PRIVATE + ACC_FINAL, "hi", "Ljava/lang/String;", null, null);
+fv.visitEnd();
+}
+{
+mv = cw.visitMethod(ACC_PUBLIC, "hi", "()Ljava/lang/String;", null, null);
+mv.visitCode();
+mv.visitVarInsn(ALOAD, 0);
+mv.visitFieldInsn(GETFIELD, "B", "hi", "Ljava/lang/String;");
+mv.visitInsn(ARETURN);
+mv.visitMaxs(1, 1);
+mv.visitEnd();
+}
+{
+mv = cw.visitMethod(ACC_PUBLIC, "A$_setter_$hi_$eq", "(Ljava/lang/String;)V", null, null);
+mv.visitCode();
+mv.visitVarInsn(ALOAD, 0);
+mv.visitVarInsn(ALOAD, 1);
+mv.visitFieldInsn(PUTFIELD, "B", "hi", "Ljava/lang/String;");
+mv.visitInsn(RETURN);
+mv.visitMaxs(2, 2);
+mv.visitEnd();
 }
 {
 mv = cw.visitMethod(ACC_PUBLIC, "hello", "()Ljava/lang/String;", null, null);
@@ -36,6 +59,8 @@ mv = cw.visitMethod(ACC_PUBLIC, "<init>", "()V", null, null);
 mv.visitCode();
 mv.visitVarInsn(ALOAD, 0);
 mv.visitMethodInsn(INVOKESPECIAL, "java/lang/Object", "<init>", "()V");
+mv.visitVarInsn(ALOAD, 0);
+mv.visitMethodInsn(INVOKESTATIC, "A$class", "$init$", "(LA;)V");
 mv.visitVarInsn(ALOAD, 0);
 mv.visitLdcInsn("Hello B!");
 mv.visitFieldInsn(PUTFIELD, "B", "hello", "Ljava/lang/String;");
--- src/main/scala/HelloScala.scala
+++ src/main/scala/HelloScala.scala
@@ -2,36 +2,26 @@ import scala.reflect.runtime.universe._
 import scala.tools.reflect._
 import scala.tools.reflect.ToolBox
 
-
-class B extends {//MyRecord { //Error: class type required but AnyRef{val hello: String} found
+trait A {
+  val hi = "Hello A!"
+}
+/*
+class B extends A {
   val hello = "Hello B!"
 }
-
+*/
 object HelloScala {
-  type MyRecord = { val hello: String}
+  type t = {val hi: String; val hello: String}
   def main(args: Array[String]): Unit = {
-    //val b = new B
+   // val b = new B
+   //   println(b.hello)
+   //   println(b.hi)
+    val B = DumpLoader.loadClass("B", BDump.dump())
+    val Bctor = B.getDeclaredConstructor()
+    val b = Bctor.newInstance().asInstanceOf[A]
+    println(b)
+    println(B.getDeclaredField("hi"))   
 
-  val b = DumpLoader.loadClass("B", BDump.dump())
-    //println(b.hello) //value hello is not a member of Class[?0]
-    val b_ = b.getDeclaredConstructor().newInstance().asInstanceOf[MyRecord]
-    //println(b_.hello)	//Value hello is not a member of Any
-    val box = new Box[MyRecord](b_)
-
-   // struc(new B)
-    struc(b_)
-  //  println(new MyRecord)  //Error: class type required but AnyRef{val hello: String} found
+  }
   
-  
-  }
-
-  def struc(b: MyRecord):Unit = {
-    println(b.hello)
-    //println(classOf[MyRecord]) //Error: class type required but AnyRef{val hello: String} found
-  }
-
-
-  class Box[B](x: B) {
-        
-  }
 }
